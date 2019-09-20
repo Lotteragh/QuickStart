096b1c89ab0633d42f75cf932b8f9c284b4c9374
exh23637 <ed.x.howard@gsk.com>
2019-09-20 12:27:14 +0100

    modify master

--- QuickStart.java
+++ QuickStart.java
@@ -5,6 +5,7 @@
 class QuickStart {
     public static void main(String[] args) {
         System.out.println("Hello World, you do look great today.");
-        System.out.println("i'm playing with featured here");
+        System.out.println("i'm playing with featured here, learning....");
+        System.out.println("always learning....");
     }
 }
