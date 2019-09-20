07ef3aeaf7794a141801e8f18f62b83fd8139e9b
exh23637 <ed.x.howard@gsk.com>
2019-09-20 12:17:09 +0100

    first ex commit

--- QuickStart.java
+++ QuickStart.java
@@ -7,4 +7,10 @@ class QuickStart {
         System.out.println("Hello World, you do look great today.");
         System.out.println("i'm playing with featured here");
     }
+
+    private void foo() {
+        System.out.println("oh what to do here, we'll think of some thing");
+    }
+
+    
 }
