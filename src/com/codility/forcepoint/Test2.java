402f19234c561142746533d388c32f3548c1f7f7
EdScratchPad <ed.howard.dev@gmail.com>
2019-10-02 17:18:11 +0100

    Fixed test 2

--- src/com/codility/forcepoint/Test2.java
+++ src/com/codility/forcepoint/Test2.java
@@ -1,5 +1,7 @@
 package com.codility.forcepoint;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Test2 {
 
 
@@ -7,81 +9,56 @@ public class Test2 {
         Test2 test = new Test2();
         //test.solve("baaaaa");
         //test.solve("baaabbaabbbaa");
-        System.out.println(test.solutionA("baaaaa"));
+        System.out.println(test.solution("aaaaaaaaa"));
     }
 
 
-    int solutionA(String S){
+    public int solution(String S) {
         int result = 0;
-        if(S.indexOf("aaa") ==-1 & S.indexOf("bbb")==-1)
+        if (!S.contains("aaa") & !S.contains("bbb"))
             return 0;
         //flush A's
         int indexOfTripleA = -1;
-        while((indexOfTripleA =S.indexOf("aaa"))!=-1){
-            String preString = S.substring(0, indexOfTripleA );
-            String postString = S.substring( indexOfTripleA +3, S.length());
-
-            if(preString.length() > 2 & !preString.substring(preString.length()-2, preString.length()).equals("bb") ){
-                S = preString + "baa" + postString;
+        while ((indexOfTripleA = S.indexOf("aaa")) != -1) {
+            if (S.length() == indexOfTripleA + 3) {
+                S = S.substring(0, indexOfTripleA) + "aab";
                 result++;
             }
+            if (S.length() > indexOfTripleA + 3) {
+                if (S.charAt(indexOfTripleA + 4) == 'a') {
+                    S = S.substring(0, indexOfTripleA) + "aab" + S.substring(indexOfTripleA + 3);
+                    result++;
+                    continue;
+                }
+                if (S.charAt(indexOfTripleA + 4) == 'b') {
+                    S = S.substring(0, indexOfTripleA) + "aba" + S.substring(indexOfTripleA + 3);
+                    result++;
+                }
 
-            if(postString.length() > 2 & !postString.substring(0, 2).equals("bb") ){
-                S = preString + "aab" + postString;
-                result++;
             }
-
-            if(preString.length() > 2 & preString.substring(preString.length()-2, preString.length()).equals("bb") & postString.length() > 2 & postString.substring(0, 2).equals("bb") ){
-                S = preString + "aba" + postString;
-                result++;
-            }
-
         }
-        //flush B
+
         int indexOfTripleB = -1;
-        while((indexOfTripleB =S.indexOf("bbb"))!=-1){
-            String preString = S.substring(1, indexOfTripleB );
-            String postString = S.substring( indexOfTripleB +3, S.length());
-
-            if(preString.length() > 2 & !preString.substring(preString.length()-2, preString.length()).equals("aa") ){
-                S = preString + "abb" + postString;
+        while ((indexOfTripleB = S.indexOf("bbb")) != -1) {
+            if (S.length() == indexOfTripleB + 3) {
+                S = S.substring(0, indexOfTripleB) + "bba";
                 result++;
             }
-
-            if(postString.length() > 2 & !postString.substring(0, 2).equals("aa") ){
-                S = preString + "bba" + postString;
-                result++;
+            if (S.length() > indexOfTripleB + 3) {
+                if (S.charAt(indexOfTripleB + 4) == 'b') {
+                    S = S.substring(0, indexOfTripleB) + "bba" + S.substring(indexOfTripleB + 3);
+                    result++;
+                }
+                if (S.charAt(indexOfTripleB + 4) == 'a') {
+                    S = S.substring(0, indexOfTripleB) + "bab" + S.substring(indexOfTripleB + 3);
+                    result++;
+                }
             }
-
-            if(preString.length() > 2 & preString.substring(preString.length()-2, preString.length()).equals("aa") & postString.length() > 2 & postString.substring(0, 2).equals("aa") ){
-                S = preString + "bab" + postString;
-                result++;
-            }
-
         }
+
 
         return result;
     }
 
-     int solution( String solve){
-         int result = 0;
-        if(solve.indexOf("aaa") ==-1 & solve.indexOf("bbb")==-1)
-            return 0;
-         char[] characters = solve.toCharArray();
 
-         int numberOfConsecutiveChars = 1;
-         for(int i = 1 ; i < characters.length ; i++){
-            if(characters[i] == characters[i-1]){
-                numberOfConsecutiveChars++;
-            }
-
-            if(numberOfConsecutiveChars==3){
-                numberOfConsecutiveChars = 0;
-                result++;
-                characters[i-3] = (characters[i] =='a'? 'b' : 'a');
-            }
-         }
-
-         return result;
-    }
 }
